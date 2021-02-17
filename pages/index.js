3858c0e08c445b11ec81160a909b8b7b595f3d68
Adi Munawar <57015700+AdiMunawar31@users.noreply.github.com>
2021-02-18 06:14:37 +0700

    firsts

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Hey There I am, </title>
+        <title>D2Y NEXT</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Adi Munawar!</a>
+          Hello There I Am, <a href="https://nextjs.org">Adi Munawar!</a>
         </h1>
 
         <p className={styles.description}>
